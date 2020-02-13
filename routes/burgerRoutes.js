2441b5bee39261e36a851d0852c19f8673846900
rsum1 <ryanjsumpter+github@gmail.com>
2020-02-12 19:15:58 -0800

    added folder structure and basic routes

---
+++ routes/burgerRoutes.js
@@ -0,0 +1,18 @@
+moduile.exports = app => {
+  // GET all Burgers
+  app.get('/burgers', (req, res)=> {
+
+  })
+  // POST one burger
+  app.post('/burgers', (req, res)=> {
+
+  })
+  // PUT one burger
+  app.put('/burgers/:id', (req, res)=> {
+
+  })
+  // DELETE one burger
+  app.delete('/burgers/:id', (req, res)=> {
+
+  })
+}
---
+++ routes/index.js
@@ -0,0 +1,3 @@
+module.exports = app => {
+  require('./burgerRoutes.js')(app)
+}
